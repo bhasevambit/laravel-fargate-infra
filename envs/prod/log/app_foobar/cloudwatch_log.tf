aa9706ed2136f07ed9062d371a1b3a9afdbbcb14
bhasevambit <jammadoibit6123@gmail.com>
2023-04-04 17:02:06 +0900

    Only add comment to tf file

--- envs/prod/log/app_foobar/cloudwatch_log.tf
+++ envs/prod/log/app_foobar/cloudwatch_log.tf
@@ -1,9 +1,11 @@
+# /// コストインパクト低のため、削除不要 ///
 resource "aws_cloudwatch_log_group" "nginx" {
   name = "/ecs/${local.name_prefix}-${local.service_name}/nginx"
 
   retention_in_days = 90
 }
 
+# /// コストインパクト低のため、削除不要 ///
 resource "aws_cloudwatch_log_group" "php" {
   name = "/ecs/${local.name_prefix}-${local.service_name}/php"
 
