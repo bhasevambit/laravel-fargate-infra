6972c3cf1a3f6dce689b0731afec2a9cf19309ad
bhasevambit <jammadoibit6123@gmail.com>
2023-04-04 11:08:32 +0900

    Update several tf files

--- envs/prod/cicd/app_foobar/iam.tf
+++ envs/prod/cicd/app_foobar/iam.tf
@@ -44,102 +44,104 @@ resource "aws_iam_role_policy_attachment" "role_deployer_policy_ecr_power_user" 
   policy_arn = data.aws_iam_policy.ecr_power_user.arn
 }
 
-# resource "aws_iam_role_policy" "s3" {
-#   name = "s3"
-#   role = aws_iam_role.deployer.id
+# /// コストインパクト低のため、削除不要 ///
+resource "aws_iam_role_policy" "s3" {
+  name = "s3"
+  role = aws_iam_role.deployer.id
 
-#   policy = jsonencode(
-#     {
-#       "Version" : "2012-10-17",
-#       "Statement" : [
-#         {
-#           "Effect" : "Allow",
-#           "Action" : [
-#             "s3:GetObject"
-#           ],
-#           "Resource" : "arn:aws:s3:::shonansurvivors-tfstate/${local.system_name}/${local.env_name}/cicd/app_${local.service_name}_*.tfstate"
-#         },
-#         {
-#           "Effect" : "Allow",
-#           "Action" : [
-#             "s3:PutObject"
-#           ],
-#           "Resource" : "${data.aws_s3_bucket.env_file.arn}/*"
-#         },
-#       ]
-#     }
-#   )
-# }
+  policy = jsonencode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Effect" : "Allow",
+          "Action" : [
+            "s3:GetObject"
+          ],
+          "Resource" : "arn:aws:s3:::terraform-backend-353981446712/LaravelApp_Dev_Book_MyCodes_tfstateFile/${local.env_name}/cicd/app_${local.service_name}_*.tfstate"
+        },
+        # {
+        #   "Effect" : "Allow",
+        #   "Action" : [
+        #     "s3:PutObject"
+        #   ],
+        #   "Resource" : "${data.aws_s3_bucket.env_file.arn}/*"
+        # },
+      ]
+    }
+  )
+}
 
-# resource "aws_iam_role_policy" "ecs" {
-#   name = "ecs"
-#   role = aws_iam_role.deployer.id
+# /// コストインパクト低のため、削除不要 ///
+resource "aws_iam_role_policy" "ecs" {
+  name = "ecs"
+  role = aws_iam_role.deployer.id
 
-#   policy = jsonencode(
-#     {
-#       "Version" : "2012-10-17",
-#       "Statement" : [
-#         {
-#           "Sid" : "RegisterTaskDefinition",
-#           "Effect" : "Allow",
-#           "Action" : [
-#             "ecs:RegisterTaskDefinition",
-#             "ecs:ListTaskDefinitions",
-#             "ecs:DescribeTaskDefinition"
-#           ],
-#           "Resource" : "*"
-#         },
-#         {
-#           "Sid" : "PassRolesInTaskDefinition",
-#           "Effect" : "Allow",
-#           "Action" : [
-#             "iam:PassRole"
-#           ],
-#           "Resource" : [
-#             data.aws_iam_role.ecs_task.arn,
-#             data.aws_iam_role.ecs_task_execution.arn,
-#           ]
-#         },
-#         {
-#           "Sid" : "DeployService",
-#           "Effect" : "Allow",
-#           "Action" : [
-#             "ecs:UpdateService",
-#             "ecs:DescribeServices"
-#           ],
-#           "Resource" : [
-#             data.aws_ecs_service.this.arn
-#           ]
-#         },
-#         {
-#           "Sid" : "RunAndWaitTask",
-#           "Effect" : "Allow",
-#           "Action" : [
-#             "ecs:RunTask",
-#             "ecs:DescribeTasks"
-#           ],
-#           "Condition" : {
-#             "ArnEquals" : {
-#               "ecs:cluster" : data.aws_ecs_cluster.this.arn
-#             }
-#           },
-#           "Resource" : [
-#             "arn:aws:ecs:${data.aws_region.current.id}:${data.aws_caller_identity.self.id}:task-definition/${local.name_prefix}-${local.service_name}:*",
-#             "arn:aws:ecs:${data.aws_region.current.id}:${data.aws_caller_identity.self.id}:task/*"
-#           ]
-#         },
-#         {
-#           "Sid" : "GetLogEvents",
-#           "Effect" : "Allow",
-#           "Action" : [
-#             "logs:GetLogEvents"
-#           ],
-#           "Resource" : [
-#             data.aws_cloudwatch_log_group.nginx.arn,
-#             data.aws_cloudwatch_log_group.php.arn
-#           ]
-#         }
-#       ]
-#     }
-#   )
-# }
+  policy = jsonencode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Sid" : "RegisterTaskDefinition",
+          "Effect" : "Allow",
+          "Action" : [
+            "ecs:RegisterTaskDefinition",
+            //"ecs:ListTaskDefinitions",
+            //"ecs:DescribeTaskDefinition"
+          ],
+          "Resource" : "*"
+        },
+        {
+          "Sid" : "PassRolesInTaskDefinition",
+          "Effect" : "Allow",
+          "Action" : [
+            "iam:PassRole"
+          ],
+          "Resource" : [
+            data.aws_iam_role.ecs_task.arn,
+            data.aws_iam_role.ecs_task_execution.arn,
+          ]
+        },
+        {
+          "Sid" : "DeployService",
+          "Effect" : "Allow",
+          "Action" : [
+            "ecs:UpdateService",
+            "ecs:DescribeServices"
+          ],
+          "Resource" : [
+            data.aws_ecs_service.this.arn
+          ]
+        },
+        # {
+        #   "Sid" : "RunAndWaitTask",
+        #   "Effect" : "Allow",
+        #   "Action" : [
+        #     "ecs:RunTask",
+        #     "ecs:DescribeTasks"
+        #   ],
+        #   "Condition" : {
+        #     "ArnEquals" : {
+        #       "ecs:cluster" : data.aws_ecs_cluster.this.arn
+        #     }
+        #   },
+        #   "Resource" : [
+        #     "arn:aws:ecs:${data.aws_region.current.id}:${data.aws_caller_identity.self.id}:task-definition/${local.name_prefix}-${local.service_name}:*",
+        #     "arn:aws:ecs:${data.aws_region.current.id}:${data.aws_caller_identity.self.id}:task/*"
+        #   ]
+        # },
+        # {
+        #   "Sid" : "GetLogEvents",
+        #   "Effect" : "Allow",
+        #   "Action" : [
+        #     "logs:GetLogEvents"
+        #   ],
+        #   "Resource" : [
+        #     data.aws_cloudwatch_log_group.nginx.arn,
+        #     data.aws_cloudwatch_log_group.php.arn
+        #   ]
+        # }
+      ]
+    }
+  )
+}
